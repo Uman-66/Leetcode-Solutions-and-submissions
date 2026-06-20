4c89813643202f55342b1a3db01acb206779c0fb
Muhammad Rumman Aslam <213151823+Uman-66@users.noreply.github.com>
2026-06-20 19:47:55 +0500

    Time: 0 ms (100%), Space: 8.6 MB (13.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+        long ans = 0 , mid = 0;
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+
+        }
+        return ans;
+    }
+};
