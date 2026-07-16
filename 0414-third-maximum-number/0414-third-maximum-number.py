98f2f6f36d622dfd3c72c6b562311046f6dabb47
Muhammad Rumman Aslam <213151823+Uman-66@users.noreply.github.com>
2026-07-16 22:02:17 +0500

    Time: 42 ms (5.24%), Space: 19.6 MB (87.99%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,7 +1,13 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        d = set(nums)
-        if len(d)<3:
-            return max(d)
-        array = sorted(d, reverse = True)
-        return array[2]
+        first = second = third = None
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+            if first is None or num > first:
+                third, second, first = second, first, num
+            elif second is None or num > second:
+                third, second = second, num
+            elif third is None or num > third:
+                third = num
+        return third if third is not None else first
